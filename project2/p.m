bc3ed4eb4fadafc974eb3b147e88f3ea6f6d84ab
Fouad Aladhami <fouadiadhami@gmail.com>
2022-12-05 23:28:54 +0300

    fi is explained

--- project2/p.m
+++ project2/p.m
@@ -1,6 +1,6 @@
 function [outputArg1] = p(fi)
 %P Summary of this function goes here
-%   Detailed explanation goes here
+%   fi is element [0,2*pi]
 outputArg1 = (cos(3*fi/4)^8 + sin(3*fi/4)^8)^(-1/4);
 
 end
