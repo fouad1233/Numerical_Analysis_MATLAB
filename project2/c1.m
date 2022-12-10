e594c5a735435566a648e488ab81f897cce96e16
Fouad Aladhami <fouadiadhami@gmail.com>
2022-12-10 12:19:14 +0300

    c1 code is update after hermit polinom is fixed

--- project2/c1.m
+++ project2/c1.m
@@ -2,11 +2,11 @@ fi = linspace(0,2*pi,360);
 plot(fi,p(fi));
 hold on;
 %%
-N = 180;
+N = 360;
 interval = [0 2*pi];
 fis = linspace(0,2*pi,360);
 y = Hermit(@p, interval ,fis,N );
 %%
-plot(fis(1:359),y,'*','MarkerIndices',1:3:length(y))
+plot(fis,y,'*','MarkerIndices',1:3:length(y))
 %% hata
-plot(fis(1:359), (y - p(fis(1:359) )) ./ p(fis(1:359)))
+plot(fis, (y - p(fis )) ./ p(fis))
