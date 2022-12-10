22a07481ad0717f5070788ecdb09e93cb6e86549
Fouad Aladhami <fouadiadhami@gmail.com>
2022-12-10 16:00:25 +0300

    init commit

---
+++ project3/L.m
@@ -0,0 +1,6 @@
+function [outputArg1] = L(difxfi,difyfi)
+%L Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = sqrt(difxfi.^2 + difyfi.^2);
+end
+
---
+++ project3/ly.m
@@ -0,0 +1,6 @@
+function [outputArg1] = ly(L,difxfi,difyfi)
+%UNTİTLED Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = (1./L(difxfi,difyfi)).*difyfi;
+end
+
