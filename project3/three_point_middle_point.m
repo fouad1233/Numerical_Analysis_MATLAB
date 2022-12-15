cc1bb045db338c6d20fdca56d9c3f60b023d9733
Fouad Aladhami <fouadiadhami@gmail.com>
2022-12-15 21:55:25 +0300

    required files imported from project 2

---
+++ project3/three_point_middle_point.m
@@ -0,0 +1,6 @@
+function [outputArg1] = three_point_middle_point(f,x0,h)
+%THREE_POİNT_MİDDLE_POİNT Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = (1/2* h) * (f(x0+h) -f(x0-h) );
+end
+
